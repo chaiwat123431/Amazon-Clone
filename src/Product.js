a88ffc39d85f588804a96523c8f880a7eb6af316
ChaiwatAikaew <chaiwatnba@gmail.com>
2025-09-21 20:07:36 -0400

    Add checkout visual action and header basket count

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const [clicked, setClicked] = useState(false);
+
+  console.log("this is the basket >>> ", basket);
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+    setClicked(true);
+    setTimeout(() => setClicked(false), 300);
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -20,7 +42,9 @@ function Product({ id, title, image, price, rating }) {
       </div>
 
       <img src={image} alt="" />
-      <button>Add to Basket</button>
+      <button onClick={addToBasket} className={clicked ? "clicked" : ""}>
+        Add to Basket
+      </button>
     </div>
   );
 }
